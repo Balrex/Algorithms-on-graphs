9877d87a9b1a9d4af1ecd8a2840e62dc29aa2fc6
Борис <118976669+Balrex@users.noreply.github.com>
2023-03-16 00:46:13 +0300

    Update node.java

--- src/node.java
+++ src/node.java
@@ -1,9 +1,16 @@
 public class node {
-    int prev;
-    int sum;
+    int prev;// для Дейкстры
+    int sum;// для Дейкстры (для Прима и Крускала тут хранится вес ребра)
 
-    public node (int prev, int sum){
+    int next;// для Крускала
+
+    public node(int prev, int sum) {
+        this.prev = prev;
+        this.sum = sum;
+    }
+    public node(int prev, int next, int sum){
         this.prev=prev;
+        this.next=next;
         this.sum=sum;
     }
 }
